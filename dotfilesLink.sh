f2f9df79146db9024a8b333daf7da8df977f622d
oret <oretyoku@yahoo.co.jp>
2014-11-01 20:32:00 +0900

    Add git-diff-tool

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,7 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+# wget https://raw.github.com/git/git/master/contrib/diff-highlight/diff-highlight
+# chmod +x diff-highlight
+# sudo mv diff-highlight /usr/local/bin/diff-highlight
